463e758d79994699056198157d1ea1cd810c7c31
ajlennon <ajlennon@users.noreply.github.com>
2025-10-16 18:08:07 +0000

    deploy: ccf4b02f51186987d6d572ccfe1c5a1e226ce32d

--- trait.impl/core/fmt/trait.Display.js
+++ trait.impl/core/fmt/trait.Display.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["xm125_radar_monitor",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"xm125_radar_monitor/error/enum.RadarError.html\" title=\"enum xm125_radar_monitor::error::RadarError\">RadarError</a>"]]]]);
+    var implementors = Object.fromEntries([["xm125_radar_monitor",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"xm125_radar_monitor/error/enum.RadarError.html\" title=\"enum xm125_radar_monitor::error::RadarError\">RadarError</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"xm125_radar_monitor/gpio/enum.GpioDirection.html\" title=\"enum xm125_radar_monitor::gpio::GpioDirection\">GpioDirection</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"xm125_radar_monitor/gpio/enum.GpioValue.html\" title=\"enum xm125_radar_monitor::gpio::GpioValue\">GpioValue</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[317]}
+//{"start":57,"fragment_lengths":[903]}
---
+++ xm125_radar_monitor/gpio/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["GpioDirection","GpioValue"],"struct":["XM125GpioController","XM125GpioPins"]};
