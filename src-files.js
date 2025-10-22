23aa177f35e3e4dab691e12df0545aeff04d44a1
ajlennon <ajlennon@users.noreply.github.com>
2025-10-22 19:50:25 +0000

    deploy: 2faec9a6d977d2148ec6a71b5a4d87f9b38662f5

--- src-files.js
+++ src-files.js
@@ -1,2 +1,2 @@
-createSrcSidebar('[["xm125_radar_monitor",["",[],["cli.rs","device_manager.rs","error.rs","firmware.rs","gpio.rs","i2c.rs","main.rs","radar.rs"]]]]');
-//{"start":19,"fragment_lengths":[127]}
+createSrcSidebar('[["xm125_radar_monitor",["",[],["cli.rs","error.rs","firmware.rs","gpio.rs","i2c.rs","main.rs","radar.rs"]]]]');
+//{"start":19,"fragment_lengths":[107]}
